b131b81b37606c15e7e23925ee6c9e23b5f0e2bc
nakabayashiyouji <y09077175175@gmail.com>
2025-05-21 11:46:52 +0900

    Delete kadai_018 directory

--- kadai_018/KatoIchiro_Chapter18.java
+++
@@ -1,8 +0,0 @@
-package kadai_018;
-
-public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
-    @Override
-    public void eachIntroduce() {
-        System.out.println("好きな食べ物はリンゴです");
-    }
-}
