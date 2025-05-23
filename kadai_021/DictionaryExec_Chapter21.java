5b6757a870f8ae0232c622f44c1f1d3bfb27c8b5
nakabayashiyouji <y09077175175@gmail.com>
2025-05-23 17:18:37 +0900

    kadai_021を提出

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,17 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		//インスタンス
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+
+        // 単語を配列にセットする
+        String[] words = {"apple", "banana", "grape", "orange"};
+
+        // 辞書を調べるメソッドを呼び出す
+        for (String word : words) {
+        	
+  }
+ }
+}
